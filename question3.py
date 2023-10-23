e877aa8b24cdca1ad1135a51381df0ee4426def4
YoelBuzgalo <yoelbuzgalo@gmail.com>
2023-10-23 11:42:07 -0400

    Practicum 2 (Question 3)

--- question3.py
+++ question3.py
@@ -1,8 +1,28 @@
 "Imports and constants should go here."
 FILENAME = "question3.csv"
+import csv
 
 def find_suspect(filename,victim,weapon,room):
     """ Your solution goes here, see instructions for details """
+    try:
+        with open(filename) as file:
+            header = next(file) # Stores and skips the header line in the .csv file
+            csv_reader = csv.reader(file) # Initializes csv reader to read the file and store in csv_reader variable
+            # Iterate over the csv_reader, each line represents as a record
+            for record in csv_reader:
+                # If the victim, weapon and room parameters match the record's victim, weapon and room values; return its record's suspect value
+                if record[1] == victim and record[2] == weapon and record[3] == room:
+                    return record[0] # Return the suspect name
+            else:
+                # Otherwise raise a ValueError for the exception to handle the error
+                raise ValueError()
+    except FileNotFoundError:
+        print(filename, "does not exist")
+        return
+    except ValueError:
+        print("No suspect is found for the given victim, weapon and room")
+        return
+    
 
 def print_case (suspect,victim,weapon,room):
      """ Output helper function """
